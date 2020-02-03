84c8c0e5d8f40bc9e2c24634e31277ae1a117d4e
Amanda Contreras <amandaben21@aol.com>
2020-02-03 01:20:31 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,34 @@
+def sort_arra_asc(array)
+array.sort
+end
+
+
+def sort_arry_desc(array)
+  array.sort.reverse
+end
+
+def sort_arry_char_count(array)
+  array.sort_by { |word| word.length }
+end
+
+def swap_elements(a)
+  a[1], a[2] = a[2], a[1]
+  a
+end
+
+def reverse_array(array)
+  array.reverse
+end
+
+def kesha_maker(array)
+  array.each do |a|
+    a[2]= "$"
+  end
+end
+
+def find_a(array)
+  array.find_all {|a| a.start_with?("a")}
+end
+
+
 
